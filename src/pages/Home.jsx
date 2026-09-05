61e1195ad68bd3cd0ed5c30dd5f126e0cb50575b
Niki <b24ee1047@iitj.ac.in>
2026-09-05 18:33:22 +0530

    Set up basic page integration and navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Home Page</h1>
+        <p>Welcome to our e-commerce website.</p>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
